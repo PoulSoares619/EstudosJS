a224b71f170eea418543a4bb58416844c39426da
Poul <poul.pheez2@gmail.com>
2022-10-27 15:01:08 -0300

    Otimizando com For

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -4,25 +4,15 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll ('.tecla');
 
-let contador  = 0;
 
-while (contador < listaDeTeclas.length) {
+for ( let contador  = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = listaDeTeclas[contador].classList[1];
-
-    
-
-    const idAudio = `#som_${instrumento}`;
-
-   //console.log(idAudio);
+    const idAudio = `#som_${instrumento}`; //template string
 
     listaDeTeclas [contador].onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador = contador + 1;
-
-    //console.log(contador)
 
 }
